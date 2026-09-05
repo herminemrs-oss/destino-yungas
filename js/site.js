3667cca1e6b75b30404affd21daf86e6ca6ce125
herminemrs-oss <herminemrs@gmail.com>
2026-09-05 20:41:51 -0300

    V3 textos PDF: actividades, itinerarios lluvias, fauna, Santa Ana, Termas, Calilegua

--- js/site.js
+++ js/site.js
@@ -5,6 +5,7 @@ document.querySelectorAll("[data-nav]").forEach((el) => {
       <a href="que-son-las-yungas.html">Qué son</a>
       <a href="mejor-epoca.html">Cuándo ir</a>
       <a href="como-llegar.html">Cómo llegar</a>
+      <a href="actividades.html">Actividades</a>
       <a href="itinerarios.html">Itinerarios</a>
       <a href="parque-nacional-calilegua.html">Calilegua</a>
       <a href="termas-rio-jordan.html">Termas</a>
